7e6399c50a67b8c4ddc7f57701f8e8a347c44bab
Josh Cook <joshuacook0023@gmail.com>
2019-08-20 08:55:25 -0400

    fix: removed commented out remove line

--- filter_bad_images.r
+++ filter_bad_images.r
@@ -9,7 +9,7 @@ for (img_file in img_files) {
     img <- try(image_read(img_file))
     if (class(img) == "try-error") {
         cat("removing currupt file:", img_file, "\n")
-        # file.remove(img_file)
+        file.remove(img_file)
     } else if (image_info(img)$format == "WEBP") {
         cat("removing WEBP file:", img_file, "\n")
         file.remove(img_file)
